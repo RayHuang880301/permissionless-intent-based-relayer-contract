0544eb7be3620245ecd70e8c5f1369b9c1fd2a94
Ray Huang <ray880301@gmail.com>
2023-11-19 00:12:16 +0300

    add getter

--- contracts/IPermissionlessIntentBasedRelayer.sol
+++ contracts/IPermissionlessIntentBasedRelayer.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.20;
 
 interface IPermissionlessIntentBasedRelayer {
-    event NewRelayer(address indexed relayer, string relayerMetadataUri);
-    event RelayerUpdated(address indexed relayer, string newRelayerMetadataUri);
+  event NewRelayer(address indexed relayer, string relayerMetadataUri);
+  event RelayerUpdated(address indexed relayer, string newRelayerMetadataUri);
 
-    function registerAsRelayer(string memory relayerMetadataUri) external;
-    function updateRelayerMetadataUri(string memory newRelayerMetadataUri) external;
+  function registerAsRelayer(string memory relayerMetadataUri) external;
+  function updateRelayerMetadataUri(
+    string memory newRelayerMetadataUri
+  ) external;
+  function getRelayerMetadataUri(
+    address relayer
+  ) external view returns (string memory);
 }
--- contracts/PermissionlessIntentBasedRelayer.sol
+++ contracts/PermissionlessIntentBasedRelayer.sol
@@ -4,18 +4,26 @@ pragma solidity ^0.8.20;
 import "./IPermissionlessIntentBasedRelayer.sol";
 
 contract PermissionlessIntentBasedRelayer is IPermissionlessIntentBasedRelayer {
+  mapping(address => string) internal relayerMetadataUris;
 
-    mapping(address => string) internal relayerMetadataUris;
-    
-    /// @inheritdoc IPermissionlessIntentBasedRelayer
-    function registerAsRelayer(string memory relayerMetadataUri) external {
-        relayerMetadataUris[msg.sender] = relayerMetadataUri;
-        emit NewRelayer(msg.sender, relayerMetadataUri);
-    }
+  /// @inheritdoc IPermissionlessIntentBasedRelayer
+  function registerAsRelayer(string memory relayerMetadataUri) external {
+    relayerMetadataUris[msg.sender] = relayerMetadataUri;
+    emit NewRelayer(msg.sender, relayerMetadataUri);
+  }
 
-    /// @inheritdoc IPermissionlessIntentBasedRelayer
-    function updateRelayerMetadataUri(string memory newRelayerMetadataUri) external {
-        relayerMetadataUris[msg.sender] = newRelayerMetadataUri;
-        emit RelayerUpdated(msg.sender, newRelayerMetadataUri);
-    }
+  /// @inheritdoc IPermissionlessIntentBasedRelayer
+  function updateRelayerMetadataUri(
+    string memory newRelayerMetadataUri
+  ) external {
+    relayerMetadataUris[msg.sender] = newRelayerMetadataUri;
+    emit RelayerUpdated(msg.sender, newRelayerMetadataUri);
+  }
+
+  /// @inheritdoc IPermissionlessIntentBasedRelayer
+  function getRelayerMetadataUri(
+    address relayer
+  ) external view returns (string memory) {
+    return relayerMetadataUris[relayer];
+  }
 }
